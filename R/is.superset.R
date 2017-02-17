8c1cc1ebdfe28abf42a11f10bf9769d86fc5857a
Ian Johnson <ianjjohnson@icloud.com>
2017-02-17 11:53:59 -0600

    C implementation of is_subset

--- R/is.superset.R
+++ R/is.superset.R
@@ -1,6 +1,6 @@
 #######################################################################
 # arules - Mining Association Rules and Frequent Itemsets
-# Copyright (C) 2011-2015 Michael Hahsler, Christian Buchta, 
+# Copyright (C) 2011-2015 Michael Hahsler, Christian Buchta,
 #			Bettina Gruen and Kurt Hornik
 #
 # This program is free software; you can redistribute it and/or modify
@@ -22,7 +22,7 @@
 ## arules specific set methods: is.superset, is.subset (only for itemMatrix)
 ##
 setMethod("is.superset", signature(x = "itemMatrix"),
-    function(x, y = NULL, proper = FALSE, sparse = FALSE) 
+    function(x, y = NULL, proper = FALSE, sparse = FALSE)
 	if (is.null(y)) t(is.subset(x, NULL, proper, sparse))
 	else t(is.subset(y, x, proper, sparse))
 	    )
@@ -39,7 +39,7 @@ setMethod("is.subset", signature(x = "itemMatrix"),
   function(x, y = NULL, proper = FALSE, sparse = FALSE) {
     if (length(x) == 0 || (!is.null(y) && length(y) == 0))
       return(logical(0))
-    
+
     ## y needs to be itemMatrix and x has to conform!
     if(!is.null(y)) {
       if(is(y, "associations")) y <- items(y)
@@ -48,60 +48,43 @@ setMethod("is.subset", signature(x = "itemMatrix"),
       x <- recode(x, itemLabels = il)
       y <- recode(y, itemLabels = il)
     }
-    
+
     if(sparse) return(.is.subset_sparse(x, y, proper))
-    
+
     if (is.null(y)) m <- .Call(R_crosstab_ngCMatrix, x@data, NULL, FALSE)
     else m <- .Call(R_crosstab_ngCMatrix, x@data, y@data, FALSE)
-    
+
     m <- m == size(x)
-    
-    if (proper == TRUE) 
-      if (is.null(y)) 
+
+    if (proper == TRUE)
+      if (is.null(y))
         m <- m & outer(size(x), size(x), "<")
-    else 
+    else
       m <- m & outer(size(x), size(y), "<")
-    
+
     rownames(m) <- labels(x)
     if(is.null(y)) colnames(m) <- labels(x)
     else colnames(m) <- labels(y)
-    
+
     m
   }
 )
 
 setMethod("is.subset", signature(x = "associations"),
-    function(x, y = NULL, proper = FALSE, sparse = FALSE) 
+    function(x, y = NULL, proper = FALSE, sparse = FALSE)
             is.subset(items(x), y, proper, sparse)
 )
 
-### use tidlist intersection 
-.is.subset_sparse <- function(x, y = NULL, proper=FALSE) { 
-  
+### use tidlist intersection
+.is.subset_sparse <- function(x, y = NULL, proper=FALSE) {
+
   if(is.null(y)) y <- x
-  
-  xitems <- LIST(x, decode = FALSE)
-  ylists <- LIST(as(y, "tidLists"), decode = FALSE)
-  
-  ## select tid-lists for items and do intersection
-  contained <- lapply(xitems, FUN = function(i) {
-    if(length(i)==0) ssid <- 1:nrow(y)
-    else { 
-      tidls <- unlist(ylists[i])
-      if(is.null(tidls)) ssid <- integer(0)
-      else ssid <- which(tabulate(tidls) == length(i))
-    }
-    
-    if(proper) ssid <- ssid[size(y)[ssid] > length(i)]
-    ssid
-  })
-  
-  m <- .list2ngCMatrix(contained, nrow(y))
-  
-  if(!is.null(y)) colnames(m) <- labels(y)
-  rownames(m) <- labels(x)
-  
-  m
+
+  p <- rep(0, y@Dim[1]+1)
+  i <- .Call("is_subset", x@data@p, x@data@i, x@data@dim, x@data@p, x@data@i, x@data@dim, p, PACKAGE = "arules")
+
+  return(new("ngCMatrix", p=p, i=i, Dim=c(max(i), y@Dim[1])))
+
 }
 
 
